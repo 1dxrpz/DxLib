72eda3beb2ad13819d7b75bb23ad2b712b9f4a85
1dxrpz <74268163+1dxrpz@users.noreply.github.com>
2020-11-21 17:20:30 +0300

    first test commit
    Debugging version 1.0.0

---
+++ DxLib/ConsoleApp4/DxEditors.cs
@@ -0,0 +1,63 @@
+﻿using DxLib;
+using DxLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DxEditor
+{
+	class SpriteEditor
+	{
+		static ConsoleColor PrimaryColor = COLOR.WHITE;
+		static ConsoleColor SecondaryColor = COLOR.RED;
+		static public void Run()
+		{
+			Scene SpriteEditorScene = new Scene();
+			Layer GUI = new Layer();
+
+			SpriteEditorScene.Add(GUI);
+			var border = new GameObject();
+			border.color = ConsoleColor.White;
+			border.position.Set(0, 16);
+			border.width = 17;
+			border.height = 1;
+
+			var border2 = new GameObject();
+			border2.color = ConsoleColor.White;
+			border2.position.Set(16, 0);
+			border2.width = 1;
+			border2.height = 16;
+
+			GUI.Add(border);
+			GUI.Add(border2);
+
+			var cursor = new GameObject();
+			cursor.width = 2;
+			cursor.height = 2;
+			cursor.position.Set(2, 2);
+			cursor.color = ConsoleColor.Red;
+
+			GUI.Add(cursor);
+
+			while (true)
+			{
+				var key = Console.ReadKey().Key;
+				if (key == ConsoleKey.LeftArrow)
+					cursor.position.x--;
+				if (key == ConsoleKey.RightArrow)
+					cursor.position.x++;
+				if (key == ConsoleKey.UpArrow)
+					cursor.position.y--;
+				if (key == ConsoleKey.DownArrow)
+					cursor.position.y++;
+				SpriteEditorScene.Render();
+			}
+		}
+		static public void ChangeColors()
+		{
+			ConsoleColor temp = PrimaryColor;
+			PrimaryColor = SecondaryColor;
+			SecondaryColor = temp;
+		}
+	}
+}
---
+++ DxLib/ConsoleApp4/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using DxLib;
+using System.Collections.Generic;
+using DxLib.Utils;
+using DxEditor;
+
+namespace ConsoleApp4
+{
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.CursorVisible = false;
+			Console.SetWindowSize(240, 60);
+
+			//DxEditor.SpriteEditor.Run();
+			
+			var scene = new Scene();
+
+			var layer1 = new Layer();
+			var layer2 = new Layer();
+			var layer3 = new Layer();
+
+			scene.Add(layer2);
+			scene.Add(layer1);
+			scene.Add(layer3);
+
+			GameObject player = new GameObject();
+			layer1.Add(player);
+			player.position = new Vector2(10, 5);
+			player.width = 10;
+			player.height = 10;
+			player.color = ConsoleColor.Green;
+
+			GameObject player1 = new GameObject();
+			layer1.Add(player1);
+			player1.position = new Vector2(20, 5);
+			player1.width = 10;
+			player1.height = 10;
+			player1.color = ConsoleColor.Magenta;
+
+			GameObject test = new GameObject();
+			layer2.Add(test);
+			test.position.Set(10, 6);
+			test.width = 5;
+			test.height = 5;
+			test.color = ConsoleColor.Red;
+
+			GameObject test2 = new GameObject();
+			layer3.Add(test2);
+			test2.position.Set(20, 6);
+			test2.width = 5;
+			test2.height = 5;
+			test2.color = ConsoleColor.White;
+
+			GameObject test3 = new GameObject();
+			layer3.Add(test3);
+			test3.position.Set(20, 16);
+			test3.width = 5;
+			test3.height = 5;
+			test3.color = ConsoleColor.Blue;
+
+			//Theatre.GetOverallInfo();
+
+			Label t = new Label();
+			t.text.Add("Player");
+			t.text.Add("dxrpz - 100%");
+			layer1.Add(t);
+
+			scene.Render();
+			while (true)
+			{
+				var key = Console.ReadKey().Key;
+				if (key == ConsoleKey.LeftArrow)
+					player.Translate(-1, 0);
+				if (key == ConsoleKey.RightArrow)
+					player.Translate(1, 0);
+				if (key == ConsoleKey.UpArrow)
+					player.Translate(0, -1);
+				if (key == ConsoleKey.DownArrow)
+					player.Translate(0, 1);
+
+				if (key == ConsoleKey.A)
+					player1.Translate(-1, 0);
+				if (key == ConsoleKey.D)
+					player1.Translate(1, 0);
+				if (key == ConsoleKey.W)
+					player1.Translate(0, -1);
+				if (key == ConsoleKey.S)
+					player1.Translate(0, 1);
+
+
+				t.position = player.position.AddTo(2, -2);
+				scene.Render();
+			}
+			
+		}
+	}
+}
